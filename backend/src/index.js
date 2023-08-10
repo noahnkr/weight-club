61ee3e153559e99bdacb36a571026f30875da3a2
Noah Roberts <noahkrob97@gmail.com>
2023-08-10 03:02:15 -0500

    Switched to Firestore

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,31 +1,23 @@
 const express = require('express');
 const app = express();
+const { database } = require('./firebase');
 
-const admin = require('firebase-admin');
-const serviceAccount = require('../service-key.json');
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://weight-club-e16e5-default-rtdb.firebaseio.com/'
+app.use(express.json());
+
+app.post('/availability/checkIn', async (req, res) => {
+    const { time, count } = req.body;
+    const ref = database.collection('weightclub').doc('availability');
+    await ref.set({
+        time: time,
+        count: count
+    });
+    res.status(200).send('Successfully posted to database')
 });
-
-const database = admin.database();
-
-app.post('/create', async (req, res) => {
-    try {
-        const newData = req.body;
-        await database.ref('/').push(newData);
-        res.status(200).send('Database updated successfully')
-
-    } catch (err) {
-        console.log('Error updating database: ', err);
-        res.status(500).send('Error updating databse');
-    }
-});
-
 
 const port = process.env.PORT || 4000;
 const host = 'localhost';
+
 app.listen(port, () => {
     console.log(`App listening at http://%s:%s`, host, port);
 });
